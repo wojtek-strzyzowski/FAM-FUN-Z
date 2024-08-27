a02d35629a61a35b2bd68e1e9d45005afe571fbb
[github-wojtek-strzyzowski] <[github-strzyzowski@gmx.de]>
2024-08-27 09:02:23 +0200

    router test

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -28,19 +28,19 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Fortify::loginView(function () {
-            return view('auth.login');
-        });
+        // Fortify::loginView(function () {
+        //     return view('auth.login');
+        // });
     
-        // Register View
-        Fortify::registerView(function () {
-            return view('auth.register');
-        });
+        // // Register View
+        // Fortify::registerView(function () {
+        //     return view('auth.register');
+        // });
     
-        // Email Verification View
-        Fortify::verifyEmailView(function () {
-            return view('auth.verify-email');
-        });
+        // // Email Verification View
+        // Fortify::verifyEmailView(function () {
+        //     return view('auth.verify-email');
+        // });
 
         Fortify::createUsersUsing(CreateNewUser::class);
         Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
